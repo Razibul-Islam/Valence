7cffb37beb8822345c66ce29405d1f378da6026b
Razibul Islam <razibulislam665@gmail.com>
2022-12-29 04:52:42 +0600

    post method change

--- src/Pages/Home/Feed/Post/PostModal.jsx
+++ src/Pages/Home/Feed/Post/PostModal.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AiOutlineLeft } from "react-icons/ai";
 import { AuthContext } from "../../../AuthProvider/AuthProvider";
 import { BsFillImageFill } from "react-icons/bs";
 import { BiSend } from "react-icons/bi";
 import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 
 const PostModal = () => {
   const { user } = useContext(AuthContext);
@@ -12,23 +13,56 @@ const PostModal = () => {
   const imgbb = process.env.REACT_APP_imagebbAPI;
 
   const submit = (data) => {
-    // console.log(data);
-    const image = data.image[0];
-    // console.log(image);
-    const formData = new FormData();
-    formData.append("image", image);
-    // console.log(formData);
-    const url = `https://api.imgbb.com/1/upload?key=${imgbb}`;
-    fetch(url, {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((imgbb) => {
+    if (data.image.length > 0) {
+      const image = data.image[0];
+      const formData = new FormData();
+      formData.append("image", image);
+      const url = `https://api.imgbb.com/1/upload?key=${imgbb}`;
+      fetch(url, {
+        method: "POST",
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then(async (imgbb) => {
           if (imgbb.success) {
-            // console.log(imgbb);
-        }
-      });
+            //   setImgLink(imgbb)
+            const details = {
+              message: data.message,
+              image: imgbb.data.url,
+            };
+            fetch("http://localhost:5000/posts", {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(details),
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                // console.log(data);
+                reset();
+                toast.success("Your post has been posted");
+              });
+          }
+        });
+    } else {
+      const details = {
+        message: data.message,
+      };
+      fetch("http://localhost:5000/posts", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(details),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          //   console.log(data);
+          reset();
+          toast.success("Your post has been posted");
+        });
+    }
   };
 
   return (
